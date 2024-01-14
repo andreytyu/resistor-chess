5840bc8e5450930afa655a810c7fe7c02a6026b6
Andrey Tyukavin <geotyukavin@gmail.com>
2024-01-14 01:41:48 +0100

    chess here we go

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import os
 import time
+import chess
 import requests
 
 chat_id = token=os.environ.get('CHAT_ID')
 token = os.environ.get('BOT_TOKEN')
+
+board = chess.Board()
+
+board2 = '''♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+⭘ ⭘ ⭘ ⭘ ⭘ ⭘ ⭘ ⭘
+⭘ ⭘ ⭘ ⭘ ⭘ ⭘ ⭘ ⭘
+⭘ ⭘ ⭘ ⭘ ⭘ ♙ ⭘ ⭘
+⭘ ⭘ ⭘ ⭘ ⭘ ⭘ ⭘ ⭘
+♙ ♙ ♙ ♙ ♙ ⭘ ♙ ♙
+♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖'''
+
+for move in board.legal_moves:
+    board_ = board.copy()
+    board_.push(move)
+    if board_.unicode() == board2:
+        break
+
+text = board.unicode() + '\n\n' + move.uci() + ' was the move' + '\n\n' + board2
 
 def send_msg(text):
    url_req = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
@@ -12,5 +32,5 @@ def send_msg(text):
    print(results.json())
 
 while True:
-    send_msg("I'm alive!")
+    send_msg(text)
     time.sleep(60*60)
