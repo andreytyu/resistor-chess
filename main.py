f4c3d1b1aac2b377543efa547a8cdcebb370626a
Andrey Tyukavin <geotyukavin@gmail.com>
2024-01-21 20:14:49 +0100

    2 channel test

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ import RPi.GPIO as GPIO
 i2c = busio.I2C(board.SCL, board.SDA)
 print("I2C addresses:", [hex(device_address) for device_address in i2c.scan()])
 # Define GPIO pins for address selection (S0-S3) and enable (E)
+reference_voltage = 4.9
 address_pins = [26, 19, 13, 6]  # Replace with your actual GPIO pin numbers
-enable_pin = 5  # Replace with your actual GPIO pin number
+known_resistor_values = [560,680,1000,2000,2200,4700,5600,10000,22000,47000,100000,1000000]
+
 S0_PIN = 26
 S1_PIN = 19
 S2_PIN = 13
@@ -45,10 +47,11 @@ ads = ADS.ADS1115(i2c, address=0x48, gain=2/3)
 print("ADS1115 Configuration:", ads)
  
 # Define the analog input channel
-channel = AnalogIn(ads, ADS.P0)
+channel0 = AnalogIn(ads, ADS.P0)
+channel1 = AnalogIn(ads, ADS.P1)
 #print("Analog Value:", channel.value, "Voltage:", channel.voltage) 
 
-
+'''
 select_channel(12)
 
 reference_voltage = 0
@@ -58,11 +61,7 @@ for x in range(50):
     reference_voltage += v
 reference_voltage = reference_voltage / 50
 print(reference_voltage)
-
-
-known_resistor_values = [1,10,47,100,220,560,1000,4700,10000,47000,100000,1000000]  # 1 kΩ resistor as an example
-known_resistor_values = [2000,2200,5600,22000,680,560,1000,4700,10000,47000,100000,1000000]  # 1 kΩ resistor as an example
-known_resistor_values = [560,680,1000,2000,2200,4700,5600,10000,22000,47000,100000,1000000]
+'''
 
 #4.096
 def read_resistance(voltage, known_value):
@@ -74,18 +73,20 @@ def read_resistance(voltage, known_value):
 
 try:
     while True:
-        fin_error = 0.5
-        resistance = 0
+        fin_error0 = 0.5
+        resistance0 = 0
+        fin_error1 = 0.5
+        resistance1 = 0
         for mux_channel in range(12):
            # enable_mux()
             #select_channel(12)
             #reference_voltage = channel.voltage
-            reference_voltage = 4.9
             select_channel(mux_channel)
             #set_channel(mux_channel)
             #voltage = channel.voltage
             #enable_mux()
-            voltage = channel.voltage
+            voltage0 = channel0.voltage
+            voltage1 = channel1.voltage
             #print("Channel {}".format(mux_channel))
             #print("Voltage: " + str(round(voltage,3)))
             #print("Channel {}".format(mux_channel))
@@ -93,14 +94,23 @@ try:
             ohms = read_resistance(voltage, known_resistor_values[mux_channel])
             error = known_resistor_values[mux_channel] - ohms
             error_percent = round((known_resistor_values[mux_channel] - ohms) / known_resistor_values[mux_channel] * 100, 1)
+            error_percent = (known_resistor_values[mux_channel] - ohms) / known_resistor_values[mux_channel]
+            if abs(error_percent) < fin_error0:
+                fin_error0 = abs(error_percent)
+                resistance0 = known_resistor_values[mux_channel]
+
+            ohms = read_resistance(voltage1, known_resistor_values[mux_channel])
+            error = known_resistor_values[mux_channel] - ohms
+            error_percent = round((known_resistor_values[mux_channel] - ohms) / known_resistor_values[mux_channel] * 100, 1)
+            error_percent = (known_resistor_values[mux_channel] - ohms) / known_resistor_values[mux_channel]
+            if abs(error_percent) < fin_error1:
+                fin_error1 = abs(error_percent)
+                resistance1 = known_resistor_values[mux_channel]
             #print("Known resistance: " + str(known_resistor_values[mux_channel]))
             #print("Channel {}: resistance {}".format(mux_channel, round(ohms)))
             #print("Error: {} Ohm, {}% ".format(error, error_percent))
             #print("\n")
-            error_percent = (known_resistor_values[mux_channel] - ohms) / known_resistor_values[mux_channel]
-            if abs(error_percent) < fin_error:
-                fin_error = abs(error_percent)
-                resistance = known_resistor_values[mux_channel]
+            
             #print("Known resistance: " + str(known_resistor_values[mux_channel]))
             #print("Channel {}: resistance {}".format(mux_channel, round(ohms)))
             #print("Error: {} Ohm, {}% ".format(error, error_percent))
@@ -111,8 +121,12 @@ try:
             #disable_mux()
         #print('='*20)
         #time.sleep(5)
-        if resistance != 0:
-            print("It's a {} Ohm resistor, error {}%".format(resistance, round(fin_error*100,2)))
+        if resistance0 != 0:
+            print("Channel 1 {} Ohm, error {}%".format(resistance0, round(fin_error0*100,2)))
+        else:
+            print("No resistor")
+        if resistance1 != 0:
+            print("Channel 1 {} Ohm, error {}%".format(resistance1, round(fin_error1*100,2)))
         else:
             print("No resistor")
         time.sleep(1)
